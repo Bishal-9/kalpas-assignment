d32fd88d0e570b2a0294b120e158d37ac2e8c023
Bishal9 <goonbishal.bg@gmail.com>
2021-09-14 13:14:56 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1>Kalpas - Assignment</h1>
+
+            {/* Left Section */}
+            <div className="app__leftSection"></div>
+
+            {/* Right Section */}
+            <div className="app__rightSection"></div>
+        </div>
+    );
 }
 
 export default App;
